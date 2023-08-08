bde92551688bd30969df1d1db189a070be340fb9
dmartin5150 <dmartin5150@icloud.com>
2023-08-08 09:22:45 -0500

    fixed message card

--- src/components/messageCard/messageCard.tsx
+++ src/components/messageCard/messageCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./messageCard.scss";
 import classnames from 'classnames';
 import Popup from "../popup/popup-component";
@@ -22,16 +22,21 @@ const closePopupHandler = () => {
 
 
 return (
-    <Popup className={classnames("popup",{open:classIsOpen === 'open'})}>
-    <div className={classnames("messagecard",{open:classIsOpen === 'open'})}>
-        <a href="#" className="messagecard__close" onClick={closePopupHandler}>
-          &times;
-        </a>
-      <h2 className="teamcard__heading">
-        <span className="teamcard__heading--main">{message}</span>
-      </h2>
+    <Fragment>
+    <div className='message-card-container'>
+      <Popup className={classnames("popup",{open:classIsOpen === 'open'})}> 
+      <div className={classnames("messagecard",{open:classIsOpen === 'open'})}>
+          <a href="#" className="messagecard__close" onClick={closePopupHandler}>
+            &times;
+          </a>
+        <h2 className="teamcard__heading">
+          <span className="teamcard__heading--main">{message}</span>
+        </h2>
+      </div>
+    </Popup> 
     </div>
-  </Popup> )
+    </Fragment>
+ )
 
 }
 
