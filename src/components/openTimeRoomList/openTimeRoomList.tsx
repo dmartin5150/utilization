2d1a53eb44a888c3d34b43c93b7a8ae398ef67f5
dmartin5150 <dmartin5150@icloud.com>
2023-08-12 14:10:09 -0500

    added logic for allselected and clear all check boxes

--- src/components/openTimeRoomList/openTimeRoomList.tsx
+++ src/components/openTimeRoomList/openTimeRoomList.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React,{useEffect,useState} from "react";
 import "./openTimeRoomList.scss"
 import ListSelector from "../ListSelector/ListSelector";
 import { useSelector } from 'react-redux';
@@ -15,6 +15,9 @@ import { setOpenTimeRoomList } from "../../store/Facility/facilty.actions";
 
 const OpenTimeRoomList = () => {
 
+    const [allRoomsSelected, setAllRoomsSelected] = useState(true)
+
+
     const selectedUnit = useSelector(selectUnit);
     const unitRoomLists = useSelector(selectUnitRoomLists);
     const roomData = useSelector(selectOpenTimeRoomHours)
@@ -23,27 +26,45 @@ const OpenTimeRoomList = () => {
     
     const dispatch = useAppDispatch()
 
-    useEffect(() => {
-        console.log('opentime changed')
-    },[openTimeRoomList])
+
+    const checkAllRoomsSelected = () => {
+
+        const unselectedItem = openTimeRoomList.findIndex((item) => item.selected === false);
+        if (unselectedItem === -1)
+            return true
+        return false;
+
+    }
 
 
     const onRoomChanged = (id:string):void => {
-        console.log('room changed')
-        console.log('id', id)
-        console.log('list', openTimeRoomList)
         const itemIndex = openTimeRoomList.findIndex((item)=> item.id.toString() === id);
         if (itemIndex !== -1) {
             openTimeRoomList[itemIndex].selected = !openTimeRoomList[itemIndex].selected;
         }
         dispatch(setOpenTimeRoomList([...openTimeRoomList]))
+        if (checkAllRoomsSelected()) {
+            setAllRoomsSelected(true)
+        } else {
+            setAllRoomsSelected(false)
+        }
+    }
+
+    const onAllRoomsSelected = () => {
+        openTimeRoomList.forEach((openTime) => openTime.selected = true)
+        dispatch(setOpenTimeRoomList([...openTimeRoomList]))
+        setAllRoomsSelected(true)
+    }
+
+    const onClearAllRooms = () => {
+        openTimeRoomList.forEach((openTime) => openTime.selected = false)
+        dispatch(setOpenTimeRoomList([...openTimeRoomList]))
+        setAllRoomsSelected(false)
     }
 
 
 
-
     useEffect(() => {
-        console.log('reseting room list')
         if (activeRoomList && activeRoomList.length !== 0) {
             const newList = activeRoomList.slice(0)
             dispatch(setOpenTimeRoomList(newList))
@@ -52,16 +73,15 @@ const OpenTimeRoomList = () => {
 
 
     const newRender = () => {
-        console.log('rerendering')
         return (       
         <div className='open-time-room-list'>
             <ListSelector 
                 itemList={openTimeRoomList} 
-                allItemsSelected={false} 
+                allItemsSelected={allRoomsSelected} 
                 displayType={ITEM_DISPLAY_TYPE.checkbox}
                 onItemChanged={onRoomChanged}
-                onAllItemsSelected={()=>{}}
-                onClearAllSelected={()=>{}}
+                onAllItemsSelected={onAllRoomsSelected}
+                onClearAllSelected={onClearAllRooms}
             />
         </div>)
     }
