4e9fd3c8f44a1392d217a38bc9ddb8502eb73b05
dmartin5150 <dmartin5150@icloud.com>
2023-08-14 11:20:59 -0500

    added styling to open time table

--- src/components/openTimeDisplay/openTimeDisplay.tsx
+++ src/components/openTimeDisplay/openTimeDisplay.tsx
@@ -13,21 +13,24 @@ export interface OpenTimeDisplayProps {
 const OpenTimeDisplay: React.FC<OpenTimeDisplayProps>  = ({openTimeData}) => {
     return (
         <div className='open-time_display'>
-            <div className='open-time-display--room'>
-                <h3 className='column-heading'>Room</h3>
-            </div>
-            <div className='open-time-display--header'>
-                <h3 className='column-heading'>Name</h3>
-                <h3 className='column-heading'>Start</h3>
-                <h3 className='column-heading'>End</h3>
-                <h3 className='column-heading'>Total</h3>
-                <h3 className='column-heading'>Release</h3>
+            <div className="open-time-display--header-container">
+                <div className='open-time-display--room'>
+                    <h3 className='column-heading'>Room</h3>
+                </div>
+                <div className='open-time-display--header'>
+                    <h3 className='column-heading'>Name</h3>
+                    <h3 className='column-heading'>Start</h3>
+                    <h3 className='column-heading'>End</h3>
+                    <h3 className='column-heading'>Total</h3>
+                    <h3 className='column-heading'>Release</h3>
+                </div>
             </div>
             {openTimeData.map((openTime, idx) => {
                 return(
-                    <div className='open-time-display--room'>
-                        <h3 className='column-heading'>{openTime.room}</h3>
-                            return <OpenTimePanel key={idx} data={openTime.data} />
+                    <div className='open-time-display--room-data'>
+                        <div className='open-time-display--panel-container'>
+                        <OpenTimePanel key={idx} room={openTime.room} data={openTime.data} />
+                        </div>
                     </div>)})}
         </div>
     )
--- src/components/openTimeDisplay/openTimePanel.tsx
+++ src/components/openTimeDisplay/openTimePanel.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import { OpenTimeDisplayInfo } from '../../store/Facility/facility.types';
+import classnames from 'classnames';
+import "./openTimePanel.scss"
 
 export interface OpenTimePanelProps {
     data: OpenTimeDisplayInfo[]
+    room:string;
 }
 
-const OpenTimePanel:React.FC<OpenTimePanelProps> = ({data}) => {
+
+
+
+
+const OpenTimePanel:React.FC<OpenTimePanelProps> = ({room, data}) => {
     return(
         data.map((item,idx) => {
+            let roomName;
+            if (idx === 0){
+                roomName = room
+             } else {
+                roomName = " "
+             }
+            //  console.log(idx, data.length)
             return (
-            <div key={idx}>
-                <h3 className='column-heading'>{item.name}</h3>
-                <h3 className='column-heading'>{item.local_start_time}</h3>
-                <h3 className='column-heading'>{item.local_end_time}</h3>
-                <h3 className='column-heading'>{item.formatted_minutes}</h3>
-                <h3 className='column-heading'>{item.release_date}</h3>
+            <div key={idx} className='open-time-panel'>
+                <div className={classnames('open-time-display--room-container',
+                        {notopborders: idx !== 0},
+                        {addbottomborder: idx === (data.length-1)})}>
+                    <h3 className={classnames('column-heading',
+                    {notopborders: roomName === " "},
+                    {addbottomborder: idx === (data.length)})}>{roomName}</h3>
+                </div>
+                <div className='open-time-panel--container'>
+                    <h3 className={classnames('column-heading',{notopborders: idx !== 0})}>{item.name}</h3>
+                    <h3 className={classnames('column-heading',{notopborders: idx !== 0})}>{item.local_start_time}</h3>
+                    <h3 className={classnames('column-heading',{notopborders: idx !== 0})}>{item.local_end_time}</h3>
+                    <h3 className={classnames('column-heading',{notopborders: idx !== 0})}>{item.formatted_minutes}</h3>
+                    <h3 className={classnames('column-heading',{notopborders: idx !== 0})}>{item.release_date}</h3>
+                </div>
             </div>)
         })
     )
