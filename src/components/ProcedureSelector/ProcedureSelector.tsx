d6d6173ed27c0a822b12afaaebdacbed63116c23
dmartin5150 <dmartin5150@icloud.com>
2023-09-05 22:07:54 -0500

    completed procedure selection UI

--- src/components/ProcedureSelector/ProcedureSelector.tsx
+++ src/components/ProcedureSelector/ProcedureSelector.tsx
@@ -4,6 +4,7 @@ import { SelectorMenuItem } from '../../store/Stats/stats.types';
 import {SingleValue} from "react-select";
 import {selectProcedures, selectRoomStats} from "../../store/Stats/stats.selector"
 import { useSelector } from "react-redux";
+import DatePicker from 'react-datepicker';
 import "./ProcedureSelector.scss"
 
 
@@ -12,6 +13,7 @@ const ProcedureSelector = () => {
     const [procedureList, setProcedureList] = useState<SelectorMenuItem[]>([])
     const [selectedProcedure, setSelectedProcedure] = useState<SingleValue<SelectorMenuItem>>({id:"0", value:"", label:""})
     const [procedureDuration, setProcedureDuration] = useState(0)
+    const [curDate, setCurDate] = useState<Date>(new Date())
 
     const curProcedures = useSelector(selectProcedures)
     const curRoomStats = useSelector(selectRoomStats)
@@ -30,15 +32,18 @@ const ProcedureSelector = () => {
                 label: procedure}
             })
             setProcedureList(curList)
-            setSelectedProcedure(curList[0])
+            if (curList.length !== 0) {
+                setSelectedProcedure(curList[0])
+                console.log('selected procedure ',curList[0] )
+            }
         }   
     },[curProcedures])
 
     useEffect(() => {
         if (selectedProcedure && selectedProcedure.value != ""  &&  curRoomStats.length !== 0) {
             const curProcedure = curRoomStats.filter((roomStat) => roomStat.procedureName == selectedProcedure.value)
-            console.log('curProcedure', curProcedure)
-            if (curProcedure[0].duration) {
+            if ((curProcedure.length !== 0) && curProcedure[0].duration) {
+                console.log('curProcedure duration', curProcedure[0].duration)
                 setProcedureDuration(curProcedure[0].duration)
             }
             
@@ -58,23 +63,38 @@ const ProcedureSelector = () => {
         setProcedureDuration(parseInt(e.target.value))
     }
 
+    const handleDateChange = (date:Date) => {
+        if (date) {
+            setCurDate(date)
+        }
+    }
 
     return(
         <div className='procedure_selector'>
             <div className="selector">
-                <SelectorList
+                {selectedProcedure && selectedProcedure.value && <SelectorList
                     title={'Select Procedure'} 
                     showBorder={false}
                     selectedOption={selectedProcedure} 
                     optionList={procedureList}
                     onChange={handleProcedureChanged}
-                    />
+                    />}
             </div>
             <div className='searchBox'>
                 <h2>Duration</h2>
                 <input value={procedureDuration} type="number" min={0}
                     onChange={handleDurationChanged}
                 />
+            </div>
+            <div className='date-selector'>
+                <h2>Date</h2>
+                <DatePicker
+                selected={new Date()} 
+                onChange={handleDateChange} 
+                />
+            </div>
+            <div className='submit-button'>
+                <button>Submit</button>
             </div>
         </div>
     )
